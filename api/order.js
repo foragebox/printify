189b2203f4396c314bd1945632a43cb93dfab78a
foragebox <info@foragebox.co.uk>
2026-04-18 14:29:38 +0100

    Create order.js

---
+++ api/order.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    console.log("Incoming order:", req.body);
+
+    return res.status(200).json({ success: true });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+}
