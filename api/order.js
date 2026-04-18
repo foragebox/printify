b95abfb4d050c4789e47b1bcc7193e4d16d862bc
foragebox <info@foragebox.co.uk>
2026-04-18 14:53:57 +0100

    Update order.js

--- api/order.js
+++ api/order.js
@@ -1,3 +1,10 @@
+const PRODUCT_MAP = {
+  "69e38990c6908e6c1cf02113": {
+    product_id: 123456,   // ← your Printify product ID
+    variant_id: 654321    // ← your Printify variant ID
+  }
+};
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
